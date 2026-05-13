e60bf7494a6b7e51dad9edf94a744812c316b6d1
Po-Ptitjo <169030407+Po-Ptitjo@users.noreply.github.com>
2026-05-13 12:58:58 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
         background_color: '#07090F',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: './',
-        scope: './',
+        start_url: '/running-app/',
+        scope: '/running-app/',
         icons: [
           { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
